d316c0ae1587ee7d644de64c88477e17cdf067ea
dieterdeweirdt <dieter@deweirdt.be>
2018-07-03 11:12:55 +0200

    Add 'DB'

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ restService.post("/story", function(req, res) {
   var about = "";
   var typeOfStory = "story";
   var location = "";
+  var mood = "";
 
   if(req && req.body && req.body.queryResult && req.body.queryResult.parameters) {
     var params = req.body.queryResult.parameters;
@@ -44,29 +45,35 @@ restService.post("/story", function(req, res) {
     var about = (params.Gender) ? ' about a ' + params.Gender.toLowerCase() : '';
     var typeOfStory = (params.TypeOfStory) ? params.TypeOfStory.toLowerCase() : 'story';
     var location = (params.City) ? ' located near ' + params.City.toLowerCase() : '';
+    var mood = (params.Mood) ? params.Mood.toLowerCase() : '';
   }
   
+  var number_of_stories = audio_list.length;
+  var x = Math.floor((Math.random() * number_of_stories));
+
+  var audio = ' <break time="500ms"/> told by ' + audio_list[x]['name'] + ' <break time="1s"/> <audio src="' + audio_list[x]['src'] +'">Audio failed to load</audio>';
+
   switch (audience) {
     //Speech Synthesis Markup Language //https://www.w3.org/TR/speech-synthesis/#S3.2.3
     case "me":
       speech =
-        '<speak>Let me tell a ' + typeOfStory + ' only for you' + about + location + ' <audio src="https://deweirdt.be/tellastory/jennifer.mp3">Audio failed to load</audio></speak>';
+        '<speak>Let me tell a ' + mood + typeOfStory + ' only for you' + about + location + audio + ' </speak>';
       break;
     case "us":
       speech =
-        '<speak>Hi you all, Let me tell a ' + typeOfStory  + about + location + ' <audio src="https://deweirdt.be/tellastory/astrid.mp3">Audio failed to load</audio></speak>';
+        '<speak>Hi you all, Let me tell a ' + mood + typeOfStory + about + location + audio  + ' </speak>';
       break;
     case "family":
       speech =
-        '<speak>Hi family, let me tell a ' + typeOfStory + about + location + ' <audio src="https://deweirdt.be/tellastory/jennifer.mp3">Audio failed to load</audio></speak>';
+        '<speak>Hi family, let me tell a ' + mood + typeOfStory + about + location + audio + ' </speak>';
       break;
     case "kids":
       speech =
-        '<speak>Hi kids, i will tell a ' + typeOfStory + '' + about + location + '</speak>';
+        '<speak>Hi kids, i will tell a ' + typeOfStory + '' + about + location + audio + '</speak>';
       break; 
     case "any":
       speech =
-        '<speak>Hi any, i will tell a ' + typeOfStory + '' + about + location + ' <audio src="https://deweirdt.be/tellastory/astrid.mp3">Audio failed to load</audio></speak>';
+        '<speak>Hi any, i will tell a ' + mood + typeOfStory + '' + about + location + audio + ' </speak>';
       break; 
   }
   return res.json({
@@ -96,3 +103,39 @@ restService.post("/story", function(req, res) {
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
+
+
+var audio_list = [
+  {
+    "name":   "Jennifer", 
+    "src":    "https://deweirdt.be/tellastory/jennifer.mp3",
+    "mood":   "fun"
+  },
+  {
+    "name":   "Astrid", 
+    "src":    "https://deweirdt.be/tellastory/astrid.mp3",
+    "mood":   "love"
+  },
+];
+
+function filterByProperty(array, prop, value){
+    var filtered = [];
+    for(var i = 0; i < array.length; i++){
+
+        var obj = array[i];
+
+        for(var key in obj){
+            if(typeof(obj[key] == "object")){
+                var item = obj[key];
+                if(item[prop] == value){
+                    filtered.push(item);
+                }
+            }
+        }
+
+    }    
+
+    return filtered;
+
+}
+
